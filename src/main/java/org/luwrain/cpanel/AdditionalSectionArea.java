816743c9c6d6c217dfcc13e8cc1fe9c8b55590c3
Michael Pozhidaev <msp@luwrain.org>
2026-05-17 18:57:01 +0700

    The forgotten file

---
+++ src/main/java/org/luwrain/cpanel/AdditionalSectionArea.java
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+// Copyright 2012-2026 Michael Pozhidaev <msp@luwrain.org>
+
+package org.luwrain.cpanel;
+
+import org.luwrain.core.*;
+
+public interface AdditionalSectionArea extends Area
+{
+}
